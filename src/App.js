b3657d36473af30c7055a33414ddc1da233cc72d
Amrutha832 <amruthatcs029@gmail.com>
2024-06-12 17:29:41 +0530

    changes commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { useRef } from 'react'
+ const App = () => {
+  const data=useRef(null);
+  const submitHandler=e=>{
+ e.preventDefault();
+ console.log(data.current.value)
+ }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ useEffect(()=>{
+data.current.focus();
+ },[])
+  return (st
+    <div>
+      <form onSubmit={submitHandler}>
+      <center>
+     <input ref={data} type="text" placeholder="enter your name "/> <br/>
+     <input type="Submit" />
+     </center>
+     </form>
     </div>
-  );
+  )
 }
 
 export default App;
+//Sure, the useRef hook is a React hook used for creating a mutable ref object whose .current property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.
